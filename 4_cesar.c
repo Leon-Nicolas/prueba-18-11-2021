3e8cb0d153adc03919ed71945b1f1e1cd215ca00
Leon-Nicolas <83369007+Leon-Nicolas@users.noreply.github.com>
2021-11-18 10:33:38 -0300

    Add files via upload

--- 4_cesar.c
+++ 4_cesar.c
@@ -3,11 +3,17 @@
 #include "libprueba.h"
 
 int main(int argc, char *argv[]) {
+  // char *palabras[argc-2];
+  int key = atoi(argv[1]);
+  for (int i = 2; i < argc; i++) {
+    printf("%s ", argv[i]);
+  }
+  printf("\n");
   // el primer argumento es la clave para cifrar
   // la cantidad de letras a desplazar (un entero)
-  int key = atoi(argv[1]);
   // para cada arg restante tratado como un string cada uno
   // cifrarlos con la clave e imprimir el mensaje entero cifrado
   // ejemplo: ./a.out 2 ab cd => cd ef
+  // cifrar(palabras, key);
   return 0;
 }
